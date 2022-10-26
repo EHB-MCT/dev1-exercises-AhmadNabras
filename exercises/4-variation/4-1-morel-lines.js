07c3cc706225374b7f91e61af5628971da4f3b1f
Nabras Ahamd <nabrsa.ahmad@student.ehb.be>
2022-10-26 15:05:53 +0200

    exercies 4-1/4-2

--- exercises/4-variation/4-1-morel-lines.js
+++ exercises/4-variation/4-1-morel-lines.js
@@ -10,13 +10,38 @@ let width = 600;
 let height = 300;
 
 
-drawLines();
+
+draw();
+
+//draw background box in orange with white lines. 
+//box size: x0, y0, h300, w600. 
+//Line thickness is 2. 
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "orange";
+    context.fillRect(0, 0, 600, 300);
+    drawLines();
+    drawLinesvertical();
+}
+
+
 
 function drawLines() {
 
     let step = width / horizontallines;
     for(let i = 0; i <= horizontallines; i++)
     Utils.drawLine(0 + (10 * i), 0, width - (10 * i), height);
-    Utils.drawLine(300 + i * 10, 0, 300 - i * 10, 300);
-    Utils.drawLine(0, i * 10, 600, 300 - i * 10);
+
+
+}
+
+function drawLinesvertical() {
+
+    let step = width / horizontallines;
+    for(let i = 0; i <= verticallines; i++)
+    Utils.drawLine(0, 0 + i * 10, width, height - i * 10);
+
+
 }
